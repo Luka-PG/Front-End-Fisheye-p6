e0239604efc2f3d800532d50d0b787ab017a98c2
Luka <105901329+Luka-PG@users.noreply.github.com>
2023-08-07 04:37:05 +0200

    page index fetch lesphotographes + ajouts des photos dans assets

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
+  //on vient nommer les valeurs que l'on va utiliser
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country,tagline ,price, id} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    // fetch du portrait en fonction du photographe
+    const picture = `assets/photographers/Photographers_ID_Photos/${portrait}`;
 
+    //établissement de la template que l'on va utiliser pour chaque photographe
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute( "href", id )
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        img.setAttribute("alt", name)
+        const a = document.createElement('a')
+        a.setAttribute('href', './photographer.html' + `?id=${id}`)
+        const names = document.createElement( 'h2' );
+        names.textContent = name;
+        const cities = document.createElement( 'h3' )
+        cities.textContent = city + ', ' + country;
+        const taglines = document.createElement( 'h4' )
+        taglines.textContent = tagline
+        const prices = document.createElement( 'p' )
+        prices.textContent = price + '€/jour'
+        article.appendChild(a)
+        a.appendChild(img);
+        a.appendChild(names);
+        article.appendChild(cities);
+        article.appendChild(taglines);
+        article.appendChild(prices);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
