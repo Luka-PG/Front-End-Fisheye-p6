8f04b1a7b887c0e75b39f1836077929469948c5b
Luka <105901329+Luka-PG@users.noreply.github.com>
2023-08-13 03:23:16 +0200

    page d'accueil finalisée

--- scripts/components/photographer.js
+++ scripts/components/photographer.js
@@ -7,27 +7,37 @@ function photographerTemplate(data) {
 
     //établissement de la template que l'on va utiliser pour chaque photographe
     function getUserCardDOM() {
+      
         const article = document.createElement( 'article' );
         article.setAttribute( "href", id )
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         img.setAttribute("alt", name)
+
         const a = document.createElement('a')
         a.setAttribute('href', './photographer.html' + `?id=${id}`)
+        a.setAttribute("aria-label", `lien vers le portfolio de ${name}`);
+
         const names = document.createElement( 'h2' );
         names.textContent = name;
+
         const cities = document.createElement( 'h3' )
         cities.textContent = city + ', ' + country;
+
         const taglines = document.createElement( 'h4' )
-        taglines.textContent = tagline
+        taglines.textContent = tagline;
+
         const prices = document.createElement( 'p' )
         prices.textContent = price + '€/jour'
+
         article.appendChild(a)
         a.appendChild(img);
         a.appendChild(names);
         article.appendChild(cities);
         article.appendChild(taglines);
         article.appendChild(prices);
+
         return (article);
     }
     return { name, picture, city, country, tagline, price, getUserCardDOM }
